ad73b13157004eee9621dcca3b874aeba9f0785d
liviaalvzs <liv.alvessantana@gmail.com>
2024-12-29 20:30:18 -0300

    update ports (5001 order, 5002 user)

--- order-service/app.py
+++ order-service/app.py
@@ -6,4 +6,4 @@ def get_users():
     return {"users": ["user1", "user2", "user3"]}
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5001, debug=True)
--- user-service/app.py
+++ user-service/app.py
@@ -68,4 +68,4 @@ def update_user(id):
     return jsonify(user.to_dict()), 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5002, debug=True)
