79da242763b24ee6cf5e4699312c0895bf930fd6
liviaalvzs <liv.alvessantana@gmail.com>
2024-12-29 11:54:28 -0300

    add order-service base code

---
+++ order-service/app.py
@@ -0,0 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/users')
+def get_users():
+    return {"users": ["user1", "user2", "user3"]}
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
